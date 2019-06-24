074686f004bda5e13c75f923cc299e43a5bff59a
krikaz <pascal.bou@protonmail.com>
2019-06-24 14:58:17 +0200

    created conditional log in

--- lambdafit/src/components/home.js
+++ lambdafit/src/components/home.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-	let retrievedObject = { message: '' };
-
 	if (localStorage.getItem('user')) {
-		retrievedObject = JSON.parse(localStorage.getItem('user'));
+		const retrievedObject = JSON.parse(localStorage.getItem('user'));
 		return (
 			<div>
 				<p>{retrievedObject.message}</p>
@@ -13,8 +12,12 @@ export default function Home() {
 	} else {
 		return (
 			<div>
-				<p>New user? Please register</p>
-				<p>Existing user? Please log in</p>
+				<p>
+					New user? Please <Link to="/register">register</Link>
+				</p>
+				<p>
+					Existing user? Please <Link to="/login">log in</Link>
+				</p>
 			</div>
 		);
 	}
