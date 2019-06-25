820aad04de386c18f38390915deb9488457b9ebd
krikaz <pascal.bou@protonmail.com>
2019-06-25 13:52:31 +0200

    removed unused code

--- lambdafit/src/components/user.js
+++ lambdafit/src/components/user.js
@@ -2,10 +2,6 @@ import React from 'react';
 import UserInfo from './userInfo';
 
 export default function User(props) {
-	const LogginProps = () => {
-		console.log(props.user[0]);
-	};
-
 	const checkUserInfo = () => {
 		props.checkUserInfo(props.id);
 	};
@@ -13,8 +9,6 @@ export default function User(props) {
 	return (
 		<div>
 			<button onClick={checkUserInfo}>Check my info</button>
-
-			<button onClick={LogginProps}>Log my info</button>
 
 			<UserInfo {...props} />
 		</div>
